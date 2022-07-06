1a344c990c5bea37bf0857b6a951cc7566886f12
sketchomania <vaibhavvast6529@gmail.com>
2022-07-06 12:31:48 +0530

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-const root = createRoot(document.getElementById("root"));
-
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorkerRegistration.register();
